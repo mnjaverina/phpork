d5e9237fb4574960416feb3b89fa0cc827c906e1
bcrivera <bcrivera@up.edu.ph>
2016-06-15 16:51:48 +0800

    "Logout" in pages/index.php is working

--- pages/index.php
+++ pages/index.php
@@ -12,18 +12,18 @@ session_start();
     <meta charset="utf-8"> 
     <meta name="viewport" content="width=device-width, initial-scale=1"> 
     <title>Pork Traceability System</title> 
-    <link rel="stylesheet" href="<?php echo HOST;?>/phpork/css/bootstrap.css"> 
-    <link rel="stylesheet" href="<?php echo HOST;?>/phpork/css/bootstrap.min.css"> 
-    <link rel="stylesheet" href="<?php echo HOST;?>/phpork/css/bootstrap-theme.css"> 
-    <link rel="stylesheet" href="<?php echo HOST;?>/phpork/css/bootstrap-theme.min.css"> 
-    <link rel="stylesheet" href="<?php echo HOST;?>/phpork/css/style2_nonnavbar.css"> 
-    <script src="<?php echo HOST;?>/phpork/js/jquery-latest.min.js" type="text/javascript"></script> 
+    <link rel="stylesheet" href="../css/bootstrap.css"> 
+    <link rel="stylesheet" href="../css/bootstrap.min.css"> 
+    <link rel="stylesheet" href="../css/bootstrap-theme.css"> 
+    <link rel="stylesheet" href="../css/bootstrap-theme.min.css"> 
+    <link rel="stylesheet" href="../css/style2_nonnavbar.css"> 
+    <script src="../js/jquery-latest.min.js" type="text/javascript"></script> 
   </head> 
   <body> 
     <div class="page-header"> 
-      <img class="img-responsive" src="<?php echo HOST;?>/phpork/css/images/Header1.png"> 
+      <img class="img-responsive" src="../css/images/Header1.png"> 
     </div> 
-    <form class="form-horizontal col-xs-10 col-sm-10 col-md-10 col-lg-10"  method="post" action="/phpork/out" style="width:50%;float:right;"> 
+    <form class="form-horizontal col-xs-10 col-sm-10 col-md-10 col-lg-10"  method="post" action="logout.php" style="width:50%;float:right;"> 
       <div class="form-group logout" > 
         <input type="text" class="col-xs-6 col-sm-5" readonly style="text-align: left; border: 2px solid; border-color: #83b26a;" value="<?php echo $_SESSION['uname'];?>"> 
         <div class="col-xs-1 col-sm-1" style="left: -1%;"> 
@@ -34,12 +34,12 @@ session_start();
     <div class="menu"> 
       <div class="menu_view"> 
         <a href="/phpork/farm">
-          <img class="img-responsive" src="<?php echo HOST;?>/phpork/css/images/View.png">
+          <img class="img-responsive" src="../css/images/View.png">
         </a> 
       </div> 
       <div class="menu_insert"> 
         <a href="/phpork/addpig">
-          <img class="img-responsive" src="<?php echo HOST;?>/phpork/css/images/Insert.png">
+          <img class="img-responsive" src="../css/images/Insert.png">
         </a> 
       </div> 
     </div> 
