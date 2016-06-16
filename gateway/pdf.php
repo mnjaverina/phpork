722e36c8180cddb7f9d783bd9556b0c9e4027a86
mnjaverina <mnjaverina@gmail.com>
2016-06-16 10:47:10 +0800

    placed correct table width measurements in  createPDFTable function in pdf.php

--- gateway/pdf.php
+++ gateway/pdf.php
@@ -8,61 +8,39 @@
 		public function createPDFTable($label, $data)
 		{
 
-				// Colors, line width and bold font
 		    $this->SetFillColor(255,0,0);
 		    $this->SetTextColor(255);
 		    $this->SetDrawColor(128,0,0);
 		    $this->SetLineWidth(.3);
 		    $this->SetFont('','B');
-		    // Header
+		  
 
-		    $w = array(40, 35, 40, 45);
-		    for($i=0;$i<count($header);$i++)
-		        $this->Cell($w[$i],7,$header[$i],1,0,'C',true);
+		    $width = array(45, 45, 40, 35, 45, 45, 45);
+		    for($i=0;$i<count($label);$i++)
+		        $this->Cell($width[$i],7,$label[$i],1,0,'C',true);
 		    $this->Ln();
-		    // Color and font restoration
+		   
 		    $this->SetFillColor(212,212,212);
 		    $this->SetTextColor(0);
 		    $this->SetFont('');
-		    // Data
+		    
 		    
 		    $fill = false;
 		    foreach($data as $row)
 		    {
-		        $this->Cell($w[0],6,$row[0],'LR',0,'L',$fill);
-		        $this->Cell($w[1],6,$row[1],'LR',0,'L',$fill);
-		        $this->Cell($w[2],6,number_format($row[2]),'LR',0,'L',$fill);
-		        $this->Cell($w[3],6,$row[3],'LR',0,'L',$fill);
-		        $this->Cell($w[4],6,date_format($row[4]),'LR',0,'R',$fill);
-		        $this->Cell($w[5],6,date_format($row[5]),'LR',0,'R',$fill);
-		        $this->Cell($w[6],6,date_format($row[6]),'LR',0,'R',$fill);
+		        $this->Cell($width[0],6,$row[0],'LR',0,'L',$fill);
+		        $this->Cell($width[1],6,$row[1],'LR',0,'L',$fill);
+		        $this->Cell($width[2],6,number_format($row[2]),'LR',0,'L',$fill);
+		        $this->Cell($width[3],6,$row[3],'LR',0,'L',$fill);
+		        $this->Cell($width[4],6,date_format($row[4]),'LR',0,'R',$fill);
+		        $this->Cell($width[5],6,date_format($row[5]),'LR',0,'R',$fill);
+		        $this->Cell($width[6],6,date_format($row[6]),'LR',0,'R',$fill);
 		        $this->Ln();
 		        $fill = !$fill;
 		    }
-		    // Closing line
+		  
 		    $this->Cell(array_sum($w),0,'','T');
 
-		}
-
-		public function generatePDFReport($pig)
-		{
-			$pdf = new PDF();
-			PDF_open_file($pdf, "Feed-Intake-Report.pdf");
-			PDF_begin_page($pdf, (72*8.5), (72*11));
-			$fontstyle = PDF_findfont($pdf, "Arial", "host", 1);
-			PDF_setfont($pdf, $fontstyle, 12);
-
-			//text area
-			PDF_show($pdf, "Feed Intake Report");
-			PDF_show($pdf, "Location: ");
-
-
-			$label = array("Feed Name", "Feed Type", "Quantity", "Unit", "Date Given", "Time Given","Production Date");
-			$data = ddl_feedRecord($pig);
-
-			$pdf->AddPage();
-			$pdf->createPDFTable($label, $data);
-			$pdf->Output();
 		}
 
 
